0e2b40088da3b69b23f77372d90aa8868caae443
Waldir Bustamante <waldir_9312@hotmail.com>
2022-12-11 20:41:57 -0500

    Se agregaron los archivos al repositorio

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+let guitarras = [
+    {nombre: "Gibson Les Paul Studio", precio: 3000},
+    {nombre: "Gibson Flying V", precio: 3600},
+    {nombre: "Fender Stratocaster", precio: 4500},
+    {nombre: "Fender Telecaster", precio: 4000},
+    {nombre: "Epiphone SG Special", precio: 2500},
+    {nombre: "Ibanez Gio", precio: 3200},
+]
+
