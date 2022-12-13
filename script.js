15c5522d8652930ad218d2bb111cd807e704fb01
Waldir Bustamante <waldir_9312@hotmail.com>
2022-12-12 23:43:23 -0500

    Se modificó el archivo script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let saludo = Number(prompt("Bienvenido a Teho Guitar Store. ¿Desea comprar una guitarra? \n 1. Sí \n 2. No"))
-
-
 let guitarras = [
     {nombre: "Gibson Les Paul Studio", precio: 3000},
     {nombre: "Gibson Flying V", precio: 3600},
@@ -9,4 +6,40 @@ let guitarras = [
     {nombre: "Epiphone SG Special", precio: 2500},
     {nombre: "Ibanez Gio", precio: 3200},
 ]
+
+let saludo = Number(prompt("Bienvenido a Teho Guitar Store. ¿Desea comprar una guitarra? \n 1. Sí \n 2. No"))
+switch (saludo) {
+    case 1:
+        let opcion = Number(prompt(" Tienes estas opciones: \n 1. Gibson Les Paul Studio \n 2. Gibson Flying V \n 3. Fender Stratocaster \n 4. Fender Telecaster \n 5. Epiphone SG Special \n 6. Ibanez Gio \n 7. No me gustan. Ya no deseo comprar"))
+        switch (opcion) {
+            case 1:
+                alert("El pago a realizar es de " + guitarras.precio1)
+                break
+            case 2:
+                alert("El pago a realizar es de " + guitarras.precio1)
+                break
+            case 3:
+                alert("El pago a realizar es de " + guitarras.precio1)
+                break
+            case 4:
+                alert("El pago a realizar es de " + guitarras.precio1)
+                break
+            case 5:
+                alert("El pago a realizar es de " + guitarras.precio1)
+                break
+            case 6:
+                alert("El pago a realizar es de " + guitarras.precio1)
+                break
+            case 7:
+                alert("El pago a realizar es de " + guitarras.precio1)
+                break
+            default:
+                alert("Ingrese una opción correcta")
+        }
+        break;
+    default:
+        alert ("Gracias por su visita")
+        break;
+}
+
 
